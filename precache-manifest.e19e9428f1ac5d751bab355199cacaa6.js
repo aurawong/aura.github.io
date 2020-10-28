97356a6b53253b8259d4beccd28602f57076b520
Billie <mail@billie.co>
2020-10-28 23:38:25 +0100

    DEPLOY

---
+++ precache-manifest.e19e9428f1ac5d751bab355199cacaa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a141eb04e78c142900fa28d89ce49b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242d0fc95ea3a1e9c488",
+    "url": "/static/css/main.9f0fa432.chunk.css"
+  },
+  {
+    "revision": "b1255c588217620f43f0",
+    "url": "/static/js/2.55685700.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.55685700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242d0fc95ea3a1e9c488",
+    "url": "/static/js/main.941a9432.chunk.js"
+  },
+  {
+    "revision": "e9b78b3a42733e9071bb",
+    "url": "/static/js/runtime-main.17c7128f.js"
+  },
+  {
+    "revision": "daeaf731dec30f7b361b6cffbbbc307f",
+    "url": "/static/media/avatar2.daeaf731.png"
+  }
+]);
